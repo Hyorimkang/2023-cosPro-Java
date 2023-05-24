29ac6f0078c9c2ff26e004f2025085de4b188bc1
HyorimKang <s2219@e-mirim.hs.kr>
2023-05-24 16:47:44 +0900

    필요한 조교 수

---
+++ src/week8/Solution34.java
@@ -0,0 +1,22 @@
+package week8;
+
+public class Solution34 {
+	 public int solution(int[] classes, int m) {
+	        int answer = 0;
+	        for(int i=0; i<classes.length; i++) {
+	            answer += classes[i] / m;
+	            if (classes[i] % m != 0)
+	                answer++;
+	        }
+	        return answer;
+	    }
+	    
+	    public static void main(String[] args) {
+	        Solution34 sol = new Solution34();
+	        int[] classes = {80, 45, 33, 20};
+	        int m = 30;
+	        int ret = sol.solution(classes, m);
+
+	        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+	    }
+}
